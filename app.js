1511ee1db1f80f7dd9318c2e30a16877d359dbb8
thenaseemahmad <naseemahmad244@gmail.com>
2023-07-15 15:00:14 +0530

    With cookies auth and registration

--- app.js
+++ app.js
@@ -4,67 +4,94 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+const session = require("express-session");
+const passport=require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
+
 app.use(express.static("public"));
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
-mongoose.connect("mongodb+srv://japsinnaseem:"+process.env.MONGODBPASS+"@cluster0.c9xlloe.mongodb.net/userDB",{useNewUrlParser:true},{ useUnifiedTopology: true });
+app.use(session({
+    secret:process.env.SECRET,
+    resave:false,
+    saveUninitialized:true,
+    //cookie:{secure:true}
+}))
 
-const userSchema = new mongoose.Schema({userName:String,password:String});
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb+srv://japsinnaseem:"+process.env.MONGODBPASS+"@cluster0.c9xlloe.mongodb.net/userDB",{useNewUrlParser:true},{ useUnifiedTopology: true },{useCreateIndex:true});
+
+const userSchema = new mongoose.Schema({username:String,password:String});
+
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("users",userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/",function(req,res){
     res.render("home");
-})
+});
 
 app.get("/login",function(req,res){
     res.render("login");
-})
+});
 
 app.get("/register",function(req,res){
     res.render("register");
-})
+});
+
+app.get("/secrets",function(req,res){
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }else{
+        res.redirect("login");
+    }
+});
+
+app.get("/logout",function(req,res){
+    req.logout(function(err){
+        res.redirect("/");
+    });
+    
+}); 
 
 app.post("/login",function(req,res){
-    //get username and password and check if it exist
-    console.log(req.body.username);
-    console.log(req.body.password);
-    User.findOne({userName:req.body.username},function(err,returnedUser){
-        if(!err){
-            if(returnedUser._doc.password===req.body.password){
-                res.render("secrets");
-            }
-            else{
-                res.send("user not found, Please register");
-                res.render("register");
-            }
-        }
-        else{
-            res.send(err);
+    const thisUser = new User({
+        username:req.body.username,
+        password:req.body.password
+    })
+    req.login(thisUser,function(err){
+        if(err){
+            console.log(err);
+        }else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            })
         }
     })
-})
+});
 
 app.post("/register",function(req,res){
-    //get username and password and check if it exist
-    console.log(req.body.username);
-    console.log(req.body.password);
-    User.findOne({userName:req.body.username},function(err,foundUser){
-        if(foundUser){
-            console.log("An user with same username exist, Please login");
-            res.render("register");
-        }else if(err){
-            res.send(err)
+    User.register({username:req.body.username},req.body.password,function(err, registeredUser){
+        if(err){
+            res.redirect("/register");
         }else{
-            const thisUser = new User({userName:req.body.username,password:req.body.password})
-            thisUser.save();
-            res.render("login");
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            });
         }
-    })
-})
+    });
+});
 
 
 app.listen(3000,function(){
